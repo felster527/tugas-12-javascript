eaf90685e72144f26328338ae371bcbd2e45b00a
felster527 <91239878+felster527@users.noreply.github.com>
2023-08-21 19:06:58 +0700

    Add files via upload

---
+++ Tugas12.js
@@ -0,0 +1,16 @@
+var panggilData = () => {
+    var data = ["pisang","jeruk","apel","mangga"]
+
+    console.log(data)
+
+    data.pop()
+
+    console.log(data)
+
+    data.shift()
+
+    return data
+    
+}
+
+console.log(panggilData())
